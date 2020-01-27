c0c3a65b4353b0ae5d560c30c0ed167732c53782
mgroff1 <mick.groff@gmail.com>
2020-01-27 10:21:27 -0700

    1
    1

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,28 @@
 // Test away!
+
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Controls from "./Controls";
+
+afterEach(rtl.cleanup);
+
+test("<App /> snapshot", async () => {
+  const wrapper = rtl.render(<Controls />);
+  await wrapper.queryAllByText(/lock/i);
+
+  // Saves our snapshot to disk, or checks an existing snapshot on the disk.
+  // If it looks at all different, the test will fail.
+  expect(wrapper.asFragment()).toMatchSnapshot();
+});
+
+// test("Cleared images", async () => {
+//   const wrapper = rtl.render(<Controls />);
+//   await wrapper.queryAllByText(/lock/i);
+
+//   const clear = wrapper.getByText("/clear/i");
+
+//   rtl.act(() => {
+//     rtl.fireEvent.click(clear);
+//   });
+// });
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test away
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
+afterEach(rtl.cleanup);
+
+test("<App /> snapshot", () => {
+  const wrapper = rtl.render(<Dashboard />);
+  expect(wrapper.asFragment()).toMatchSnapshot();
+});
